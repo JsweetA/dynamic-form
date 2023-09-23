c3d041e7bb07b99ae240c4f9d0694f85432ace99
黄院 <2171077189@qq.com>
2023-09-23 14:19:26 +0800

    fix:add lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,17 +1,17 @@
-import { ESLint } from "eslint";
+const { ESLint } = require("eslint");
 
-const removeIgnoredFiles = async (files) => {
+const removeIgoredFiles = async (files) => {
   const eslint = new ESLint();
   const ignoredFiles = await Promise.all(
-    files.map((file) => eslint.isPathIgnored(file)),
+    files.map((file) => eslint.isPathIgored(file)),
   );
   const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
   return filteredFiles.join(" ");
 };
 
-export default {
+module.exports = {
   "*": async (files) => {
-    const filesToLint = await removeIgnoredFiles(files);
+    const filesToLint = await removeIgoredFiles(files);
 
     return [`eslint ${filesToLint} --max-warnings=0`];
   },
