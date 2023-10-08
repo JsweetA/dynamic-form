fc1ad2148475a981c9629778ae584aa0d736da27
黄院 <2171077189@qq.com>
2023-10-08 15:28:21 +0800

    feat: 🚀 新增tsconfig和rollup.config.ts

--- packages/utils/rollup.config.ts
+++ packages/utils/rollup.config.ts
@@ -1,14 +1,18 @@
 // 导入依赖
 import { terser } from "rollup-plugin-terser";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import { RollupOptions } from "rollup";
+import ts from "rollup-plugin-typescript";
+import typescript from "typescript";
 // tsconfig.json合并选项
 // 一般来说默认使用项目的tsconfig.json，如果有个别需要修改的如下，可以在此修改
 
-export default {
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config: RollupOptions = {
   // 项目入口
-  input: "lib/index.ts",
-
+  input: "src/index.ts",
   // 打包后的出口和设置
   output: [
     {
@@ -36,5 +40,13 @@ export default {
 
   // 使用的插件
   // 注意，这里的插件使用是有顺序的，先把ts编译为js，然后查找依赖，最后压缩
-  plugins: [typescript({}), commonjs(), terser()],
+  plugins: [
+    ts({
+      typescript,
+    }),
+    commonjs(),
+    terser(),
+  ],
 };
+
+export default config;
